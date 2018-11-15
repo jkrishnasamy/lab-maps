bb9778b13436f5b85fc3dfa7b641c1aee35aa9dc
jkrishnasamy <jkrishnasamy97@gmail.com>
2018-11-14 23:38:25 -0800

    actually finish lab

--- MapLab/app/src/main/java/edu/uw/jkrishna/maplab/MapsActivity.kt
+++ MapLab/app/src/main/java/edu/uw/jkrishna/maplab/MapsActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -36,8 +37,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val fountain =  LatLng(47.653784, -122.307803)
+        mMap.addMarker(MarkerOptions().position(fountain).title("This is the fountain"))
     }
 }
